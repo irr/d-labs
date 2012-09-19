c2e7cc377c7a5a95fe96f513109ca7b33c64ad48
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2012-09-19 08:58:31 -0300

    dmd.conf

--- event.d
+++ event.d
@@ -4,6 +4,9 @@ import deimos.event2.event;
 // cd /data/D
 // git clone https://github.com/D-Programming-Deimos/libevent.git
 // dmd -I/data/D/libevent -L-levent -O event.d
+// or add to your dmd.conf
+// /etc/dmd.conf or ln -s /etc/dmd.conf /data/D/dmd2/linux/bin64/dmd.conf
+// DFLAGS=-I/data/D/libevent -L-levent -I%@P%/../../src/phobos -I%@P%/../../src/druntime/import -L-L%@P%/../lib64 -L-L%@P%/../lib32 -L--no-warn-search-mismatch -L--export-dynamic
 
 void main()
 {
